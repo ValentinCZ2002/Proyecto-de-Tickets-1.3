45db1a12fb3c19d0ce6a8b75101f0d493e3f0621
ValentinCZ2002 <chahuayllo06@gmail.com>
2022-10-05 23:29:25 -0500

    Se agrega el proyecto

---
+++ Sol_PuntoVenta/Sol_PuntoVenta.Negocio/N_SubFamilias.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Sol_PuntoVenta.Entidades;
+using Sol_PuntoVenta.Datos;
+
+namespace Sol_PuntoVenta.Negocio
+{
+    public class N_SubFamilias
+    {
+        public static DataTable Listado_sf(string cTexto)
+        {
+            D_SubFamilias Datos = new D_SubFamilias();
+            return Datos.Listado_sf(cTexto);
+        }
+
+        public static string Guardar_sf(int nOpcion, E_SubFamilias oPropiedad)
+        {
+            D_SubFamilias Datos = new D_SubFamilias();
+            return Datos.Guardar_sf(nOpcion, oPropiedad);
+        }
+
+        public static string Eliminar_sf(int Ncodigo)
+        {
+            D_SubFamilias Datos = new D_SubFamilias();
+            return Datos.Eliminar_sf(Ncodigo);
+        }
+        public static DataTable Listado_fa(string cTexto)
+        {
+            D_SubFamilias Datos = new D_SubFamilias();
+            return Datos.Listado_fa(cTexto);
+        }
+    }
+}
---
+++ Sol_PuntoVenta/Sol_PuntoVenta.Presentacion/Controles/MiMesa.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sol_PuntoVenta.Negocio;
+
+namespace Sol_PuntoVenta.Presentacion.Controles
+{
+    public partial class MiMesa : UserControl
+    {
+        public MiMesa()
+        {
+            InitializeComponent();
+        }
+        #region "Mis Variables y Propiedades"
+        public int Codigo
+        {
+            get { return Convert.ToInt32(Lbl_codigo_me.Text); }
+            set { Lbl_codigo_me.Text = Convert.ToString(value); }
+        }
+
+        public string Descripcion
+        {
+            get { return Lbl_descripcion_me.Text; }
+            set { Lbl_descripcion_me.Text= value; }
+        }
+
+        public Image Disponible
+        {
+            get { return Pct_estado.Image; }
+            set { Pct_estado.Image = value; }
+        }
+
+        public int Codigo_pv
+        {
+            get { return Convert.ToInt32(Lbl_codigo_pv.Text); }
+            set { Lbl_codigo_pv.Text = Convert.ToString(value); }
+        }
+
+        public string Descripcion_pv
+        {
+            get { return Lbl_descripcion_pv.Text; }
+            set { Lbl_descripcion_pv.Text = value; }
+        }
+
+        public int Codigo_us
+        {
+            get { return Convert.ToInt32(Lbl_codigo_us.Text); }
+            set { Lbl_codigo_us.Text = Convert.ToString(value); }
+        }
+
+        public int Codigo_tu
+        {
+            get { return Convert.ToInt32(Lbl_codigo_tu.Text); }
+            set { Lbl_codigo_tu.Text = Convert.ToString(value); }
+        }
+
+        public string Fecha_trabajo
+        {
+            get { return Lbl_fecha_trabajo.Text; }
+            set { Lbl_fecha_trabajo.Text = value; }
+        }
+        #endregion
+
+        private void Pct_imagenmesa_Click(object sender, EventArgs e)
+        {
+            Procesos.Frm_Mesa_Abierta oFrm_mesaabierta = new Procesos.Frm_Mesa_Abierta();
+            oFrm_mesaabierta.Lbl_codigo_me.Text =Convert.ToString(Codigo);
+            oFrm_mesaabierta.Txt_mesaseleccionada.Text = Descripcion;
+            oFrm_mesaabierta.Txt_puntoventa.Text = Descripcion_pv;
+            oFrm_mesaabierta.Lbl_codigo_pv.Text = Convert.ToString(Codigo_pv);
+            oFrm_mesaabierta.Lbl_codigo_tu.Text = Convert.ToString(Codigo_tu);
+            oFrm_mesaabierta.Lbl_archivo_txt.Text = Convert.ToString(DateTime.Now.Ticks);
+            oFrm_mesaabierta.Lbl_fecha_trabajo.Text = Fecha_trabajo;
+            oFrm_mesaabierta.Btn_nuevopedido.Focus();
+            try
+            {
+                oFrm_mesaabierta.Dgv_listado_sf.DataSource = N_MesaAbierta.Listar_SubFamilias_RP(Codigo_pv);
+                //Dando formato al datagridview de SubFamilia
+                oFrm_mesaabierta.Dgv_listado_sf.Columns[0].Width = 250;
+                oFrm_mesaabierta.Dgv_listado_sf.Columns[0].HeaderText = "SUBFAMILIAS";
+                oFrm_mesaabierta.Dgv_listado_sf.Columns[1].Visible = false;
+
+                oFrm_mesaabierta.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);   
+            }            
+
+        }
+    }
+}
